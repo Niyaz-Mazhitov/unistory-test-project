5ec8fcceed20e7b4a32dddaa56b1e98919756bc2
Niyaz <mniyaz.kz@gmail.com>
2023-03-01 16:02:56 +0600

    Initial commit
    Installed: react, redux-toolkit, react-redux, react-router-dom, styled-components, usedapp/core + ethers.

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './common/styles/main.css';
+import App from './app';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <App/>
+);
