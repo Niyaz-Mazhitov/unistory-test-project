9e3ab1c900fd94523c62d32d77f1e97f741052b9
Niyaz <mniyaz.kz@gmail.com>
2023-03-05 22:30:09 +0600

    Improvements

--- src/modules/basic/components/preview/index.tsx
+++ src/modules/basic/components/preview/index.tsx
@@ -1,6 +1,7 @@
-import React, {MouseEvent, useState} from 'react';
+import React, {MouseEvent, useEffect, useState} from 'react';
 import * as S from './styles';
 import planet from './planet.png';
+import {PlanetInners} from './styles';
 
 export default function Preview() {
     const [coordinates, setCoordinates] = useState<{ x: number; y: number }>({x: 110, y: 110});
@@ -16,6 +17,12 @@ export default function Preview() {
     return (
         <S.Container>
             <S.PlanetContainer x={coordinates.x} y={coordinates.y} onMouseMove={onMove} onMouseLeave={onLeave}>
+                <ProgressBar/>
+                <PlanetInners>
+                    <S.InnerRing size={446}/>
+                    <S.InnerRing isLine size={370}/>
+                    <S.InnerRing isLine size={384}/>
+                </PlanetInners>
                 <img src={planet} alt=''/>
             </S.PlanetContainer>
             <S.Title>
@@ -36,5 +43,36 @@ export default function Preview() {
                 ex ea commodo consequat.
             </S.Description>
         </S.Container>
+    );
+}
+
+function ProgressBar() {
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setProgress(47);
+        }, 800);
+    }, []);
+
+    return (
+        <S.ProgressBarContainer>
+            <S.Dots>
+                <S.TopDots>
+                    <S.Dot delay={9} backDelay={2} isShown={72 <= progress}/>
+                    <S.Dot delay={3} backDelay={3} isShown={22 <= progress}/>
+                </S.TopDots>
+                <S.BottomDot>
+                    <S.Dot delay={6} backDelay={1} isShown={47 <= progress}/>
+                </S.BottomDot>
+            </S.Dots>
+            <S.ProgressBarCircle viewBox='0 0 100 100'>
+                <S.ProgressBarForeground cx='50' cy='50' r='45' progress={progress}/>
+            </S.ProgressBarCircle>
+            <S.ProgressBarTitle>
+                <span>Q1 2022</span>
+                <div><S.Dot isShown/></div>
+            </S.ProgressBarTitle>
+        </S.ProgressBarContainer>
     );
 }
--- src/modules/basic/components/preview/styles.ts
+++ src/modules/basic/components/preview/styles.ts
@@ -1,5 +1,13 @@
 import styled, {css} from 'styled-components';
 
+export const Container = styled.div`
+  width: 1010px;
+  height: 500px;
+  position: absolute;
+  display: flex;
+  align-items: center;
+`;
+
 export const PlanetContainer = styled.div<{ x: number; y: number }>`
   left: 570px;
   width: 540px;
@@ -18,19 +26,112 @@ export const PlanetContainer = styled.div<{ x: number; y: number }>`
   }
 `;
 
-export const Container = styled.div`
-  width: 1010px;
-  height: 500px;
+export const PlanetInners = styled.div`
+  width: 540px;
+  height: 540px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const InnerRing = styled.div<{ size: number; isLine?: boolean }>`
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  position: absolute;
+  border-radius: 100%;
+  ${props => props.isLine ? 'border: 1px solid #323232' : 'background-color: #1E1E20'}
+`;
+
+export const ProgressBarContainer = styled.div`
+  top: -5px;
+  position: absolute;
+  width: 550px;
+  height: 550px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ProgressBarTitle = styled.div`
+  position: absolute;
+  top: 15px;
+  padding: 8px 12px;
+  background: #FFFFFF;
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  span {
+    height: 14px;
+    font-size: 14px;
+    color: #171719;
+    font-family: ${props => props.theme.fontFamily.primary};
+  }
+  div {
+    width: 12px;
+    height: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    border-radius: 100%;
+  }
+`;
+
+export const ProgressBarCircle = styled.svg`
+  transform: rotate(-90deg);
+`;
+
+export const ProgressBarForeground = styled.circle<{ progress: number }>`
+  fill: none;
+  stroke: #E75626;
+  stroke-width: .1;
+  stroke-dasharray: 300;
+  stroke-dashoffset: 0;
+  transition: 3s;
+  ${({progress}) => css`stroke-dashoffset: ${300 - (300 * progress) / 100};`}
+`;
+
+export const Dots = styled.div`
+  width: 501px;
+  height: 501px;
+  position: absolute;
+  top: 25px;
+`;
+
+export const Dot = styled.div<{ delay?: number; backDelay?: number; isShown: boolean }>`
+  width: 6px;
+  height: 6px;
+  max-width: 6px;
+  max-height: 6px;
+  background-color: #E75626;
+  border-radius: 100%;
+  opacity: ${props => props.isShown ? 1 : 0};
+  transition: ${props => props.isShown ? props.delay : props.backDelay}s;
+`;
+
+export const TopDots = styled.div`
+  width: 100%;
+  height: 100%;
   position: absolute;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+`;
+
+export const BottomDot = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
 `;
 
 export const Title = styled.div<{ isMask?: boolean; x?: number; y?: number }>`
   position: absolute;
   font-size: 120px;
   text-transform: uppercase;
-  transition: 1.5s;
+  transition: 1.4s;
   font-family: ${props => props.theme.fontFamily.primary};
   color: ${props => props.isMask && props.theme.color.primary};
   clip-path: ${props => props.isMask && css`circle(160px at ${props.x}px ${props.y}px)`};
